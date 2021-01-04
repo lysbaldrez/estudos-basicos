ffc67cd680c0d588fb6f2feeac9b71a444ae23dc
49-00057 <amaryllis.calefi@quicko.com.br>
2021-01-04 13:59:52 -0300

    Initial commit

---
+++ src/main/java/Caneta.java
@@ -0,0 +1,23 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar() {
+        if (this.tampada) {
+            System.out.println("ERRO! Não posso rabiscar ");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+}
